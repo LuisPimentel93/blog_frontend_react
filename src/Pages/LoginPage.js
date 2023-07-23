c3c5dde81af09e981414109d205b5fd0d99eebe4
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-07-23 18:21:32 -0400

    styled the login and signup pages

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='login'>
+        <h1>Login</h1>
+        <input type='text' placeholder='username'/>
+        <input type='password' placeholder='password'/>
+        <button>Login</button>
+
+    </form>
   )
 }
 
--- src/Pages/SignupPage.js
+++ src/Pages/SignupPage.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 const SignupPage = () => {
   return (
-    <div>
-       this is signup
-    </div>
+    <form className='signup'>
+        <h1>SignUp</h1>
+        <input type='text' placeholder='Username'/>
+        <input type='email' placeholder='Email'/>
+        <input type='text' placeholder='First name'/>
+        <input type='text' placeholder='Last name'/>
+        <input type='password' placeholder='Password'/>
+        <button>Signup</button>
+    </form>
   )
 }
-
 export default SignupPage
+
+
+
